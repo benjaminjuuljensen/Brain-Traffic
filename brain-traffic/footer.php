3cd99a177ea0452d5af57c0f70032a05b95e6996
Benjamin Juul Jensen <43336954+benjaminjuuljensen@users.noreply.github.com>
2020-10-07 09:16:02 +0200

    Update footer.php

--- brain-traffic/footer.php
+++ brain-traffic/footer.php
@@ -4,16 +4,22 @@
  *
  * Contains the closing of the #content div and all content after.
  *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package Brain_Traffic
  */
 
 ?>
-
-	<footer id="colophon" class="site-footer">
+<footer id="colophon" class="site-footer">
 		<form style="border:1px solid #ccc">
-  <div class="container">
+
+<div class="container">
     <h1>Join the Brain Traffic mailing list</h1>
     <p>Sign up for email about upcoming events, new posts, and other delightful tidbits.</p>
     <hr>
@@ -32,7 +38,24 @@
       <button type="submit" class="signupbtn">Sign Up</button>
     </div>
   </div>
+	
 </form>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+
+
+<ul>
+<li>&copy 2020 Brain Traffic</li>
+<li><a href="">Confab Conference</a></li>
+<li><a href="">Button Conference</a></li>
+<li><a href="">Content Strategy Podcast</a></li>
+<li><a href="">Contact Us</a></li>
+<li><a href="">Newsletter</a></li>
+<li><a href="">Twitter</a></li>
+<li><a href="">Newsletter</a></li>
+<li><a href="">LinkedIn</a></li>
+</ul>
 
 <?php wp_footer(); ?>
 
